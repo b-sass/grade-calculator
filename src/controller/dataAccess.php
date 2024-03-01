edc09253a956f627feba6240a4b9fc09f5fe0688
Bartosz Sass <sassbartek@gmail.com>
2024-03-01 00:06:50 +0000

    Removed database secrets lmfao

--- src/controller/dataAccess.php
+++ src/controller/dataAccess.php
@@ -1,9 +1,4 @@
 <?php 
-// $host = "sql210.infinityfree.com:3306";
-// $user = "if0_36021998";
-// $pass = "uC5FiTKBQ1ft";
-// $dbname = "if0_36021998_test";
-
 $host = "localhost";
 $user = "flare";
 $pass = "flare";
