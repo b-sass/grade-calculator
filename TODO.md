a1b26114822d772f9555edf51448a87451150f90
Lino Perdrix <k2232947@kingston.ac.uk>
2024-03-01 12:21:28 +0000

    added todo.md / edited login and signup pages

---
+++ TODO.md
@@ -0,0 +1,2 @@
+### Login page
+- [ ] User should be able to login using either username OR email
--- src/view/login.php
+++ src/view/login.php
@@ -5,22 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Flare Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/bootstrapConfig.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
-
 <body>
     <!-- top bar -->
     <div id="topBar" class="container-fluid py-3 border border-2">
         <div class="row justify-content-between align-items-center">
-            <a href="signup.php" class="col text-start d-none d-md-block text-decoration-none text-muted">
-                <i class="bi bi-arrow-left"></i>Back
+            <p class="col text-start d-none d-md-block text-decoration-none text-muted">
                 <i class="bi bi-calculator"></i>[logo placeholder]
-            </a>
-            <!-- maybe d-none d-md-block not neccessary (test with small screen size) -->
+            </p>
+            <!-- maybe d-none d-md-block not neccessary (TODO test with small screen size) -->
             <div class="col text-end">
-                <h6><a href="signup.php" class="text-decoration-none text-muted">Create an account</a></h6> <!-- this <a> style should probably be defined elsewhere/use some other class rather than add text-decoration-none text-muted each time -->
+                <h6><a href="signup.php" class="text-decoration-none">Create an account</a></h6> <!-- this <a> style should probably be defined elsewhere/use some other class rather than add text-decoration-none text-muted each time -->
             </div>
         </div>
     </div>
@@ -32,15 +29,17 @@
                 <h5 class="display-3 text-center mb-5">Login</h5>
                 <form method="post" action="../controller/userLogin.php">
                     <div id="emailDiv" class="mb-3">
-                        <label for="emailInput" class="form-label">Email</label>
+                        <label for="emailInput" class="form-label">Email/Username</label>
                         <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <a href="#" class="text-muted">Forgot Username?</a>
                     </div>
                     <div id="passwordDiv" class="mb-3">
                         <label for="password" class="form-label">Password</label>
                         <input type="password" name="password" class="form-control" id="passwordInput">
+                        <a href="#" class="text-muted">Forgot password?</a>
                     </div>
                     <div class="row align-items-center">
-                        <button id="loginButton" type="submit" href="#" class="btn btn-primary row">Login</button>
+                        <button id="loginButton" type="submit" href="#" class="btn btn-primary col">Login</button>
                     </div>
                     <div class="my-3 form-check">
                         <input type="checkbox" class="form-check-input" id="rememberMeChekbox">
