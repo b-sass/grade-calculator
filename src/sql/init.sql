7dd764791b53412f0bd8c20075a8cb55170de862
Bartosz Sass <sassbartek@gmail.com>
2024-03-11 15:44:58 +0000

    Database init files

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -1,6 +1,6 @@
 USE flare;
 
-CREATE TABLE IF NOT EXISTS User (
+CREATE TABLE User (
 	userID INT(6) NOT NULL AUTO_INCREMENT,
 	username VARCHAR(40) NOT NULL,
 	email VARCHAR(40) NOT NULL,
@@ -8,14 +8,14 @@ CREATE TABLE IF NOT EXISTS User (
 	PRIMARY KEY (userID)
 );
 
-CREATE TABLE IF NOT EXISTS Module (
-	moduleCode VARCHAR(10),
-	moduleName VARCHAR(40),
-	-- level INT(1) -- NOT NEEDED
+CREATE TABLE Module (
+	moduleCode VARCHAR(10) NOT NULL,
+	moduleName VARCHAR(60) NOT NULL,
+	level INT(1) NOT NULL,
 	PRIMARY KEY (moduleCode)
 );
 
-CREATE TABLE IF NOT EXISTS ChosenModule (
+CREATE TABLE ChosenModule (
 	userID INT(6) NOT NULL,
 	moduleCode VARCHAR(10) NOT NULL,
 	FOREIGN KEY (userID) REFERENCES User(userID),
@@ -23,24 +23,24 @@ CREATE TABLE IF NOT EXISTS ChosenModule (
 	PRIMARY KEY (userID, moduleCode)
 );
 
-CREATE TABLE IF NOT EXISTS Assignment (
+CREATE TABLE Assignment (
 	assignmentID INT(6) NOT NULL AUTO_INCREMENT,
+	assignmentName VARCHAR(60) NOT NULL,
+	assignmentWeight DEC(5,2) NOT NULL,
+	sequenceIndex INT(2),
 	moduleCode VARCHAR(10) NOT NULL,
-	assignmentName VARCHAR(40),
-	assignmentWeight DEC(5,2),
-	-- sequenceIndex INT(2) -- NOT NEEDED
 	FOREIGN KEY (moduleCode) REFERENCES Module(moduleCode),
 	PRIMARY KEY (assignmentID)
 );
 
-CREATE TABLE IF NOT EXISTS Scenario (
+CREATE TABLE Scenario (
 	scenarioID INT(8) NOT NULL AUTO_INCREMENT,
 	userID INT(6) NOT NULL,
 	FOREIGN KEY (userID) REFERENCES User(userID),
 	PRIMARY KEY (scenarioID)
 );
 
-CREATE TABLE IF NOT EXISTS Grade (
+CREATE TABLE Grade (
 	gradeID INT(6) NOT NULL AUTO_INCREMENT,
 	assignmentID INT(6) NOT NULL,
 	scenarioID INT(8) NOT NULL,
