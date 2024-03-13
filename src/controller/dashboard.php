9dd9c714a8a4cca6b8a7935766d799e5e9bd3465
Bartosz Sass <sassbartek@gmail.com>
2024-03-13 00:21:06 +0000

    Reduced repetition.

--- src/controller/dashboard.php
+++ src/controller/dashboard.php
@@ -5,9 +5,11 @@ require_once("../model/data/dataAccess.php");
 session_start();
 
 $user = $_SESSION["loggedInUser"][0];
-$modules4 = getUserModulesForLevel($user->userID, 4);
-$modules5 = getUserModulesForLevel($user->userID, 5);
-$modules6 = getUserModulesForLevel($user->userID, 6);
+$modules = array(
+			getUserModulesForLevel($user->userID, 4),
+			getUserModulesForLevel($user->userID, 5),
+			getUserModulesForLevel($user->userID, 6)
+			);
 
 require_once "../view/dashboard_view.php";
 
