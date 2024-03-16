e0ac34eb7d45daf7f61118f81dd0ffbe666b5e7c
Attiq <attiqbashir0@gmail.com>
2024-03-16 22:33:37 +0000

    Update editModules.js

--- src/scripts/editModules.js
+++ src/scripts/editModules.js
@@ -1,31 +1,36 @@
 let menus = document.querySelectorAll("select");
-let modules = document.querySelectorAll("option");
 
-modules.forEach(option => {
-	option.addEventListener('click', hideOption);
-	console.log(option.value);
-})
-
-console.log(menus[0].value);
+// Attach the 'change' event listener to each select element
+menus.forEach(menu => {
+    menu.addEventListener('change', function() {
+        hideOption();
+    });
+});
 
 function hideOption() {
-	// Go through each module and set it to enabled
-	this.selected = true;
-	modules.forEach(option => {
-		if (option.value != "") {
-			option.style.display = "initial";
-		}
-	})
+    // First, make all options visible
+    menus.forEach(menu => {
+        let options = menu.options;
+        for (let i = 0; i < options.length; i++) {
+            options[i].style.display = "block";
+        }
+    });
 
-	// Go through each menu and disable module options that have
-	// been selected already
-	menus.forEach(dropdown => {
-		modules.forEach(option => {
-			if(dropdown.value == option.value) {
-				option.style.display = "none";
-			}
-		});
-	})
+    // Then, collect all selected values except the placeholder ""
+    let selectedValues = Array.from(menus).map(menu => menu.value).filter(value => value !== "");
+
+    // Iterate over each select element
+    menus.forEach(menu => {
+        let options = menu.options;
+        // Iterate over each option in the current select
+        for (let i = 0; i < options.length; i++) {
+            // If the option's value is in the list of selected values, but not the current select's value, hide it
+            if (selectedValues.includes(options[i].value) && menu.value !== options[i].value) {
+                options[i].style.display = "none";
+            }
+        }
+    });
 }
 
-//Balls
+// Initial call to set the correct display states based on the default selected options
+hideOption();
