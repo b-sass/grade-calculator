6e24e0a2765428c8393bd8f2b24c0aa9056fce8d
Bartosz Sass <sassbartek@gmail.com>
2024-03-18 16:26:30 +0000

    Users now can't set passwords that are empty strings.

--- src/controller/accountManager.php
+++ src/controller/accountManager.php
@@ -4,16 +4,22 @@ require_once("../model/data/dataAccess.php");
 session_start();
 
 $user = $_SESSION["loggedInUser"];
+$warning = "";
 
-if(isset($_REQUEST["email"])) {
+if($_REQUEST["email"] != "") {
 	$email = $_REQUEST["email"];
 	updateEmailForUser($user->userID, $email);
+	$warning .= "<br>Email changed."; 
 }
 
-if(isset($_REQUEST["pass"])) {
+if($_REQUEST["pass"] != "") {
 	$pass = $_REQUEST["pass"];
 	updatePassForUser($user->userID, $pass);
+	$warning .= "<br>Password changed.";
 }
 
-session_destroy();
+$warning .= "<br>You have been logged out.";
+session_unset();
+$_SESSION["warning"] = $warning;
+
 require_once("../controller/home.php");
